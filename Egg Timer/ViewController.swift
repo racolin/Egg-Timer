abaa120de8db50737dd111d4bf0c409b9c634c41
tin <tin.pt@ntnc.vn>
2023-06-11 11:37:15 +0700

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Administrator on 11/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
